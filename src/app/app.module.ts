b1e672aa30cd2d94d6d078f0403337474cf76ec8
achiang7 <anthonychiang7@gmail.com>
2021-06-15 17:53:45 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { MessageBoardComponent } from './components/message-board/message-board.component';
+import { WishListComponent } from './components/wish-list/wish-list.component';
+import { MainComponent } from './components/home/main/main.component';
+import { QuotesComponent } from './components/home/quotes/quotes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    MessageBoardComponent,
+    WishListComponent,
+    MainComponent,
+    QuotesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
